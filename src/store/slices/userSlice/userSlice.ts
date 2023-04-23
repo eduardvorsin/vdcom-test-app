855571505a2e4efd4de8c2d3a037e79aad4ba64f
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-04-23 17:29:39 +0600

    added userSlice

---
+++ src/store/slices/userSlice/userSlice.ts
@@ -0,0 +1,36 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { IUser } from '../../../models/IUser';
+/* eslint-disable no-param-reassign */
+
+type UserState = {
+  username: string | null,
+  password: string | null,
+  token: string
+}
+
+const initialState: UserState = {
+  username: null,
+  password: null,
+  token: '',
+};
+
+const userSlice = createSlice({
+  name: 'auth',
+  initialState,
+  reducers: {
+    loginUser(state: UserState, action: PayloadAction<IUser>) {
+      state.username = action.payload.username;
+      state.password = action.payload.password;
+      state.token = action.payload.token;
+    },
+
+    logoutUser(state: UserState) {
+      state.username = null;
+      state.password = null;
+      state.token = '';
+    },
+  },
+});
+
+export const { loginUser, logoutUser } = userSlice.actions;
+export default userSlice.reducer;
