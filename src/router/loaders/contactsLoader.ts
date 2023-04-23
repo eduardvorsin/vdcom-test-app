62e588b833fe64c75fcf46a91e750cd42383c39a
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-04-23 17:31:24 +0600

    added contactsLoader

---
+++ src/router/loaders/contactsLoader.ts
@@ -0,0 +1,21 @@
+import {
+  redirect,
+} from 'react-router-dom';
+import store from '../../store/store';
+import { getToken, isAuth } from '../../helpers/authorization';
+import fetchContacts from '../../store/thunks/fetchContacts/fetchContacts';
+
+const contactsLoader = async () => {
+  if (!isAuth()) redirect('/login');
+  const token = getToken();
+
+  if (token) {
+    store.dispatch(fetchContacts(
+      token,
+    ));
+  }
+
+  return null;
+};
+
+export default contactsLoader;
