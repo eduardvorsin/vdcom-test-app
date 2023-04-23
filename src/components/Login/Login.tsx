0a57aee76b550c185dad1161a0fed24896ce3fd0
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-04-23 17:36:37 +0600

    added Login component

---
+++ src/components/Login/Login.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { useActionData } from 'react-router-dom';
+import { Fade, AlertTitle, Alert } from '@mui/material';
+import LoginForm from '../LoginForm/LoginForm';
+
+const Login = () => {
+  const actionError = useActionData();
+  const error = actionError instanceof Error ? actionError : '';
+
+  return (
+    <>
+      {error && (
+        <Fade
+          in={Boolean(error)}
+        >
+          <Alert
+            severity='error'
+            variant='standard'
+            sx={{
+              mb: '30px',
+            }}
+          >
+            <AlertTitle>Ошибка</AlertTitle>
+            {error.message}
+          </Alert>
+        </Fade>
+      )}
+      <LoginForm />
+    </>
+  );
+};
+
+export default Login;
