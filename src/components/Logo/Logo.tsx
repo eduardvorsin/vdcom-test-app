a4b8259cbae1b25e5fe92ffaf6d304c986f68e38
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-04-23 17:35:25 +0600

    added Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import {
+  Link,
+  SxProps,
+  Theme,
+  styled,
+} from '@mui/material';
+import { Link as RouterLink } from 'react-router-dom';
+import Logotype from '../../assets/images/icons/logo.svg?url';
+
+type LogoProps = {
+  sx?: SxProps<Theme>,
+};
+
+const StyledLogo = styled('img')(({ theme }) => ({
+  display: 'block',
+  width: '100px',
+  height: '27px',
+  [theme.breakpoints.up('sm')]: {
+    width: '110px',
+    height: '30px',
+  },
+  [theme.breakpoints.up('md')]: {
+    width: '140px',
+    height: '39px',
+  },
+  [theme.breakpoints.up('lg')]: {
+    width: '175px',
+    height: '48px',
+  },
+}));
+
+const Logo: React.FC<LogoProps> = ({
+  sx,
+}) => (
+  <Link
+    sx={{
+      display: 'inline-flex',
+      ...sx,
+    }}
+    to='/contacts'
+    component={RouterLink}
+  >
+    <StyledLogo
+      src={Logotype}
+      alt='logo'
+    />
+  </Link>
+);
+
+export default Logo;
