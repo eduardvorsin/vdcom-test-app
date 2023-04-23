12f93418c878cbdbcc98ffa38e748f98258dddec
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-04-23 17:31:55 +0600

    added changeContactAction

---
+++ src/router/actions/changeContactAction.ts
@@ -0,0 +1,42 @@
+import {
+  ActionFunctionArgs, ParamParseKey, Params, redirect,
+} from 'react-router-dom';
+import { IContact } from '../../models/IContact';
+import store from '../../store/store';
+import { getToken } from '../../helpers/authorization';
+import changeContact from '../../store/thunks/changeContact/changeContact';
+import { ContactsChangePathName } from '../AppRouter/AppRouter';
+
+interface changeContactActionArgs extends ActionFunctionArgs {
+  params: Params<ParamParseKey<ContactsChangePathName>>,
+  request: Request,
+}
+
+const changeContactAction = async ({ params, request }: changeContactActionArgs) => {
+  const contactFormData = await request.formData();
+
+  const contactData = {
+    clientId: Number(params.clientId),
+    clientName: contactFormData.get('clientName'),
+    'TRN/PPSN': contactFormData.get('TRN/PPSN'),
+    yearEnd: contactFormData.get('yearEnd'),
+    ARD: contactFormData.get('ARD'),
+    companyNumber: Number(contactFormData.get('companyNumber')),
+    email: contactFormData.get('email'),
+    phoneNumber: Number(contactFormData.get('phoneNumber')),
+    companyAdress: contactFormData.get('companyAdress'),
+  } as unknown as IContact;
+
+  const token = getToken();
+
+  if (token) {
+    await store.dispatch(changeContact({
+      data: contactData,
+      token,
+    }));
+  }
+
+  return redirect('/contacts');
+};
+
+export default changeContactAction;
