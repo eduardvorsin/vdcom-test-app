e67aaaf59c56795bb5755de7a9150dec8b5ed49e
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-04-29 13:27:10 +0600

    took out sx styles in styled component

--- src/router/pages/LoginPage/LoginPage.tsx
+++ src/router/pages/LoginPage/LoginPage.tsx
@@ -1,59 +1,76 @@
 import React from 'react';
-import { Box, Typography, Container } from '@mui/material';
+import { Box, Typography, Container, styled } from '@mui/material';
 import Login from '../../../components/UI/Login/Login';
 import Logo from '../../../components/UI/Logo/Logo';
+
+const LoginWrapper = styled(Box)(({ theme }) => ({
+  height: '100vh',
+  margin: '0 auto',
+  maxWidth: '500px',
+  minHeight: '505px',
+  display: 'flex',
+  justifyContent: 'center',
+  flexDirection: 'column',
+
+  [theme.breakpoints.up('sm')]: {
+    minHeight: '585px',
+  },
+  [theme.breakpoints.up('md')]: {
+    minHeight: '692px',
+    maxWidth: '654px',
+  },
+  [theme.breakpoints.up('lg')]: {
+    minHeight: '790px',
+  },
+}));
+
+const LoginLogo = styled(Logo)(({ theme }) => ({
+  alignSelf: 'center',
+  marginBottom: '28px',
+
+  [theme.breakpoints.up('sm')]: {
+    marginBottom: '68px',
+  },
+  [theme.breakpoints.up('md')]: {
+    marginBottom: '108px',
+  },
+  [theme.breakpoints.up('lg')]: {
+    marginBottom: '148px',
+  },
+}));
+
+const LoginTitle = styled(Typography)(({ theme }) => ({
+  lineHeight: '1.46',
+  marginBottom: '20px',
+  textAlign: 'center',
+
+  [theme.breakpoints.up('sm')]: {
+    marginBottom: '25px',
+  },
+  [theme.breakpoints.up('md')]: {
+    marginBottom: '30px',
+  },
+  [theme.breakpoints.up('lg')]: {
+    marginBottom: '35px',
+  },
+}));
 
 const LoginPage = () => (
   <Container
     maxWidth='xl'
   >
-    <Box
-      sx={{
-        height: '100vh',
-        maxWidth: { xs: '500px', md: '654px' },
-        margin: '0 auto',
-        minHeight: {
-          xs: '505px',
-          sm: '585px',
-          md: '692px',
-          lg: '790px',
-        },
-        display: 'flex',
-        justifyContent: 'center',
-        flexDirection: 'column',
-      }}
-    >
-      <Logo
-        sx={{
-          alignSelf: 'center',
-          mb: {
-            xs: '28px',
-            sm: '68px',
-            md: '108px',
-            lg: '148px',
-          },
-        }}
-      />
+    <LoginWrapper>
+      <LoginLogo />
 
-      <Typography
-        align='center'
+      <LoginTitle
         variant='h3'
         component='h1'
-        sx={{
-          lineHeight: '1.46',
-          mb: {
-            lg: '35px',
-            md: '30px',
-            sm: '25px',
-            xs: '20px',
-          },
-        }}
       >
         Welcome To CRM System Sign In To Your Account
-      </Typography>
+      </LoginTitle>
 
       <Login />
-    </Box>
+    </LoginWrapper>
   </Container>
 
 );
