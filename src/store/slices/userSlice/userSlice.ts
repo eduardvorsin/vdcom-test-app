646c66729fb9b770a6db7b0811166e6ff6594fed
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-06-08 13:31:25 +0600

    added type export

--- src/store/slices/userSlice/userSlice.ts
+++ src/store/slices/userSlice/userSlice.ts
@@ -2,7 +2,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { IUser } from '../../../models/IUser';
 /* eslint-disable no-param-reassign */
 
-type UserState = {
+export type UserState = {
   username: string | null,
   password: string | null,
   token: string
