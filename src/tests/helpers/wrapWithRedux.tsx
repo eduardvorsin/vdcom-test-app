88730fb21c88505d8e4164ddb8e464c99706b7d3
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-06-08 13:34:07 +0600

    added wrapForRedux test helper

---
+++ src/tests/helpers/wrapWithRedux.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { PreloadedState } from '@reduxjs/toolkit';
+import { RootState, setupStore } from '../../store/store';
+
+// eslint-disable-next-line max-len
+const wrapWithRedux = (component: React.ReactElement, initialState: PreloadedState<RootState>): ReturnType<typeof Provider> => {
+  const store = setupStore(initialState);
+
+  return (
+    <Provider store={store} >
+      {component}
+    </Provider>
+  );
+};
+
+export default wrapWithRedux;
