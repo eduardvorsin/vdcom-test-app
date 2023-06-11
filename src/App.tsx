138e2b660f7067927debccf4d6c8e14ac3d549f9
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-06-11 11:56:08 +0600

    selected common styles in a separate file

--- src/App.tsx
+++ src/App.tsx
@@ -197,17 +197,6 @@ theme = createTheme(theme, {
   },
 });
 
-export const visuallyHiddenStyles = {
-  position: 'absolute',
-  width: '1px',
-  height: '1px',
-  margin: -'1px',
-  padding: '0',
-  overflow: 'hidden',
-  border: '0',
-  clip: 'rect(0 0 0 0)',
-} as const;
-
 export default function App() {
   return (
     <ThemeProvider theme={theme}>
