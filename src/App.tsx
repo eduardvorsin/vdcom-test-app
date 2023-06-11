a9dca1568c78373f226b9baaafe3ea1422c80bc8
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-06-11 11:57:09 +0600

    updated styles

--- src/App.tsx
+++ src/App.tsx
@@ -43,31 +43,38 @@ theme = createTheme(theme, {
   components: {
     MuiCssBaseline: {
       styleOverrides: `
-      @font-face {
-        font-family: 'Poppins';
-        font-style: normal;
-        font-weight: 400;
-        font-display: swap;
-        src: url(${Poppins400}) format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      @font-face {
-        font-family: 'Roboto Condensed';
-        font-style: normal;
-        font-weight: 400;
-        font-display: swap;
-        src: url(${RobotoCondensed400}) format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      @font-face {
-        font-family: 'Roboto Condensed';
-        font-style: normal;
-        font-weight: 700;
-        font-display: swap;
-        src: url(${RobotoCondensed700}) format('woff2');
-        unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
-      }
-      `,
+        @font-face {
+            font-family: 'Poppins';
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+            src: url(${Poppins400}) format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+          }
+
+          @font-face {
+            font-family: 'Roboto Condensed';
+            font-style: normal;
+            font-weight: 400;
+            font-display: swap;
+            src: url(${RobotoCondensed400}) format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+          }
+
+          @font-face {
+            font-family: 'Roboto Condensed';
+            font-style: normal;
+            font-weight: 700;
+            font-display: swap;
+            src: url(${RobotoCondensed700}) format('woff2');
+            unicode-range: U+0000-00FF, U+0131, U+0152-0153, U+02BB-02BC, U+02C6, U+02DA, U+02DC, U+2000-206F, U+2074, U+20AC, U+2122, U+2191, U+2193, U+2212, U+2215, U+FEFF, U+FFFD;
+          }
+
+          body {
+            overflow-wrap: break-word;
+            font-family: 'Poppins', sans-serif;
+          }
+        `,
     },
     MuiTextField: {
       variants: [
