64b61dcd4ed9f023b677637e4714b6e08fe0ecc3
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-06-11 11:51:07 +0600

    removed unnecessary functions from the import

--- src/components/ContactForm/ContactsForm.test.tsx
+++ src/components/ContactForm/ContactsForm.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
-import { act, render, screen, waitFor } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import ContactForm from './ContactForm';
 
