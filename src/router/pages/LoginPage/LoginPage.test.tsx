f4d2159af713a96f2b32d3cb9588847b60c09a92
eduardvorsin <eduardvorsin2019@yandex.kz>
2023-06-19 12:46:50 +0600

    added integration tests to LoginPage

--- src/router/pages/LoginPage/LoginPage.test.tsx
+++ src/router/pages/LoginPage/LoginPage.test.tsx
@@ -1,8 +1,11 @@
 import React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
 import { RouterProvider, createMemoryRouter } from 'react-router-dom';
+import userEvent from '@testing-library/user-event';
 import LoginPage from './LoginPage';
+import addMemoryRouter from '../../../tests/helpers/addMemoryRouter';
+import wrapWithRedux from '../../../tests/helpers/wrapWithRedux';
 
 const routes = [{
   path: '/',
@@ -23,6 +26,38 @@ describe('LoginPage tests', () => {
   it('is a basic snapshot', () => {
     render(<RouterProvider router={router} />);
 
+    expect(screen.getByTestId<HTMLDivElement>('login-page')).toMatchSnapshot();
+  });
+});
+
+describe('LoginPage integration tests', () => {
+  it('should show an error if the login and password field is not filled in and the sign in button was pressed', async () => {
+    const user = userEvent.setup();
+    render(wrapWithRedux(addMemoryRouter({ initialEntries: ['/login'] }), {}));
+
+    await act(async () => {
+      await user.type(screen.getByLabelText<HTMLInputElement>(/login/i), 'rrooot2');
+      await user.type(screen.getByLabelText<HTMLInputElement>(/password/i), 'rrooot2');
+    });
+
+    user.click(screen.getByRole<HTMLButtonElement>('button', { name: /Sign in/i }));
+
+    expect(await screen.findByText<HTMLHeadingElement>(/Error/i)).toBeInTheDocument();
+    expect(await screen.findByText<HTMLDivElement>(/not found/i)).toBeInTheDocument();
+  });
+
+  it('is a snapshot with error message', async () => {
+    const user = userEvent.setup();
+    render(wrapWithRedux(addMemoryRouter({ initialEntries: ['/login'] }), {}));
+
+    await act(async () => {
+      await user.type(screen.getByLabelText<HTMLInputElement>(/login/i), 'rrooot2');
+      await user.type(screen.getByLabelText<HTMLInputElement>(/password/i), 'rrooot2');
+    });
+
+    user.click(screen.getByRole<HTMLButtonElement>('button', { name: /Sign in/i }));
+
+    await screen.findByText<HTMLHeadingElement>(/Error/i);
     expect(screen.getByTestId<HTMLDivElement>('login-page')).toMatchSnapshot();
   });
 });
